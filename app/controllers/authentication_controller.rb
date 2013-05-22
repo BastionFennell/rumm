fe537a65113445b2173fda56fe3c3acae3a50c37
Charles Lowell <cowboyd@thefrontside.net>
2013-05-22 11:30:35 -0500

    spike login with authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,19 @@
+require 'net/https'
+require 'json'
+
 class AuthenticationController
+
   def login
-    Object.new
+#    username = console.ask "username: "
+ #   password = console.ask_passord "password:"
+    uri = URI('https://identity.api.rackspacecloud.com/v2.0/tokens')
+    req = Net::HTTP::Post.new(uri)
+    req['Content-Type'] = 'application/json'
+    req.body = {auth: {passwordCredentials: {username: username, password: password}}}.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |https|
+      https.request req
+    end
+    Map(JSON.parse res.body)
   end
 
   def logout
