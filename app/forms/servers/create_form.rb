fdda9a8918d88dd468d810fa835c869e61d76d0e
Bastion <bastionfennell@utexas.edu>
2013-07-09 16:10:12 -0500

    Added ssh to the forms

--- app/forms/servers/create_form.rb
+++ app/forms/servers/create_form.rb
@@ -4,4 +4,36 @@ class Servers::CreateForm < MVCLI::Form
   input :name, String, default: ->() {naming.generate_name 's', 's'}
   input :image_id, String, default: '9922a7c7-5a42-4a56-bc6a-93f857ae2346'
   input :flavor_id, String, default: 2
+  input :ssh_private, String, default: ->() {File.expand_path "~/.ssh/id_rsa"}, decode: ->(s) {File.expand_path s}
+  input :ssh_public, String, default: ->() {File.expand_path "~/.ssh/id_rsa.pub"}, decode: ->(s) {File.expand_path s}
+
+  validates(:ssh_private, "private ssh key location must lead to a file") {|ssh_private| File.exists? ssh_private}
+  validates(:ssh_private, "private ssh key must be a valid key") do |ssh_private|
+    #Duct tape
+    begin
+      test = File.open(ssh_private, "r")
+      test_line = test.readline
+      test_line.delete! "\n"
+      test_line.delete! "\r"
+      test_line == "-----BEGIN RSA PRIVATE KEY-----"
+    rescue
+      false
+    end
+    #Duct tape
+  end
+
+  validates(:ssh_public, "public ssh key location must lead to a file") {|ssh_public| File.exists? ssh_public}
+  validates(:ssh_public, "public ssh key must be a valid key") do |ssh_public|
+    #Duct tape
+    begin
+      test = File.open(ssh_public, "r")
+      test_line = test.readline.split[0]
+      test_line.delete! "\n"
+      test_line.delete! "\r"
+      test_line == "ssh-rsa"
+    rescue
+      false
+    end
+    #Duct tape
+  end
 end
