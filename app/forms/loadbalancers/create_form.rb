9e8a3d68161bef18f1eef5bc837aa3d150c88275
Charles Lowell <cowboyd@thefrontside.net>
2013-07-10 16:57:37 -0500

    make form properly return attributes

--- app/forms/loadbalancers/create_form.rb
+++ app/forms/loadbalancers/create_form.rb
@@ -9,10 +9,10 @@ class Loadbalancers::CreateForm < MVCLI::Form
 
   input :protocol, String, default: 'HTTP', decode: :upcase
 
-  input :virtual_ips, [String], default: ['PUBLIC']
+  input :virtual_ips, [String], default: [Map(type: 'PUBLIC')], decode: ->(s) {Map(type: s.upcase)}
 
   input :nodes, [Node], required: true do
-    input :address, IPAddr, required: true, decode: ->(s) {IPAddr.new s}
+    input :address, String, required: true, decode: ->(s) { URI('').hostname = s }
     input :port, Integer, default: 80, decode: ->(s) {Integer s}
     input :type, String, default: 'PRIMARY', decode: :upcase
     input :condition, String, default: 'ENABLED', decode: :upcase
@@ -24,14 +24,7 @@ class Loadbalancers::CreateForm < MVCLI::Form
 
   validates(:port, "port must be between 0 and 65,535") {|port| port >=0 && port < 65535}
 
-  validates(:nodes, "at least one node must be enabled") { |nodes|
+  validates(:nodes, "at least one node must be enabled", each: false) { |nodes|
     nodes.any? {|n| n.condition == 'ENABLED'}
   }
-
-  output do |form|
-    form.attributes.merge attrs = {
-      virtual_ips: form.virtual_ips.map {|t| Map(type: t)},
-      nodes: form.nodes.map(&:attributes)
-    }
-  end
 end
