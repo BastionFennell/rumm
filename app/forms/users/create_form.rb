6d85d0cab8e9ba30ff887d88db8dcf4626bd5dbf
Charles Lowell <cowboyd@thefrontside.net>
2013-07-12 14:24:14 -0500

    make a dabatase required

--- app/forms/users/create_form.rb
+++ app/forms/users/create_form.rb
@@ -1,7 +1,7 @@
 class Users::CreateForm < MVCLI::Form
   input :name, String, required: true
   input :password, String, required: true
-  input :databases, [Map], decode: ->(s) {{name: s}}
+  input :databases, [Map], required: true, decode: ->(s) {{name: s}}
   input :host, String
 
   validates(:host, "host must be a valid ip address or %") {|host|
