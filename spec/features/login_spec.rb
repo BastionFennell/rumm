4c6fe4faad7614f2186f6bb86460dd78c4fe4094
Bastion <bastionfennell@utexas.edu>
2013-07-16 14:22:09 -0500

    Added tests for logging in

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
+require "netrc"
 
 describe "logging in" do
-  before do
+  Given do
     #@announce_dir = true
     #@announce_cmd = true
     #@announce_env = true
@@ -9,8 +10,8 @@ describe "logging in" do
     #@announce_stderr = true
     @home = Pathname(set_env "HOME", File.expand_path(current_dir))
   end
-  describe "interactively with valid credentials" do
-    before do
+  context "interactively with valid credentials" do
+    When do
       VCR.use_cassette('successful-login') do
         will_type ENV['RACKSPACE_USERNAME'] || "<username>"
         will_type ENV['RACKSPACE_PASSWORD'] || "<valid-password>"
@@ -18,11 +19,39 @@ describe "logging in" do
         stop_process @interactive
       end
     end
-    it "successfully exits" do
-      last_exit_status.should eq(0), "process failed"
-    end
-    it "places your login credentials in your .netrc" do
+    Then {last_exit_status == 0}
 
+    context "places your login credentials in your .netrc" do
+      Then do
+        n = Netrc.read
+        user, pass = n["api.rackspace.com"]
+        user != nil and pass != nil
+      end
+    end
+  end
+
+  context "logging out" do
+    When {VCR.use_cassette('successful-logout') {run "rumm logout"}}
+    Then {last_exit_status == 0}
+
+    context "removes your login credentials from .netrc" do
+      Then do
+        n = Netrc.read
+        user, pass = n["api.rackspace.com"]
+        user == nil and pass == nil
+      end
+    end
+    context "interactively with invalid credentials" do
+      When do
+        VCR.use_cassette("unsuccessful-login") do
+          will_type "nil"
+          will_type "nil"
+          run_interactive "rum login"
+          stop_process @interactive
+        end
+      end
+      Then {last_exit_status != 0}
+      And {all_stderr =~ /User could not be authenticated/}
     end
   end
 end
