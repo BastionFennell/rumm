87aa3b98deef3c73e65850f37d5ee402ed43abfb
Charles Lowell <cowboyd@thefrontside.net>
2013-07-22 13:53:31 -0500

    slightly adjust nil checks for readability

--- app/providers/naming_provider.rb
+++ app/providers/naming_provider.rb
@@ -6,10 +6,8 @@ class NamingProvider
     self
   end
 
-  def generate_name(first_letter = random_letter, last_letter = random_letter)
-    first_letter ||= random_letter
-    last_letter ||= random_letter
-    "#{adjective first_letter}-#{noun last_letter}"
+  def generate_name(adj_first_letter = nil, noun_first_letter = nil)
+    "#{adjective adj_first_letter}-#{noun noun_first_letter}"
   end
 
   def random_letter
@@ -28,11 +26,13 @@ class NamingProvider
   end
 
   def noun(first_letter)
+    first_letter ||= random_letter
     list = dictionary('nouns')[first_letter]
     list[rand(list.length - 1)]
   end
 
   def adjective(first_letter)
+    first_letter ||= random_letter
     list = dictionary('adjectives')[first_letter]
     list[rand(list.length - 1)]
   end
