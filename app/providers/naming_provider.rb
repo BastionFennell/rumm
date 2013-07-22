2582d1927dd7631fbaa7aa35072d5263632d23c9
Charles Lowell <cowboyd@thefrontside.net>
2013-07-22 11:13:27 -0500

    convert naming provider to use a pstore
    The existing method of using flat text files meant that indices had to
    be manually updated. This was causing terribleness in the form of nil
    references and infinite loops.
    
    A rake task was added to use the text files to generate a binary
    pstore indexed by letter name. This is more efficient since it is
    thunked more quickly into memory and names are reached in constant time
    
    fixes #28

--- app/providers/naming_provider.rb
+++ app/providers/naming_provider.rb
@@ -1,113 +1,37 @@
+require "pstore"
+
 class NamingProvider
 
   def value
     self
   end
 
-  def generate_name(adj = nil, noun = nil)
-    if adj == nil and noun == nil
-      get_adj + "-" + get_noun
-    else
-      get_adj_bound(adj) + "-" + get_noun_bound(noun)
-    end
+  def generate_name(first_letter = random_letter, last_letter = random_letter)
+    "#{adjective first_letter}-#{noun last_letter}"
+  end
+
+  def random_letter
+    ('a'..'z').to_a.shuffle.first
   end
 
   private
 
-
-  def adjective_file
-    File.expand_path("../naming/adj.txt", __FILE__)
+  def dictionary(key)
+    @dictionary ||= PStore.new dictionary_file
+    @dictionary.transaction(true) { @dictionary[key] }
   end
 
-  def get_adj
-    a = rand(12586)
-    counter = 0
-    File.open adjective_file do |dict|
-      dict.each_line do |line|
-        if counter == a
-          return line.chomp
-        end
-        counter+=1
-      end
-    end
-
+  def dictionary_file
+    File.expand_path '../naming/dictionary.pstore', __FILE__
   end
 
-  def get_noun
-    a = rand(20746)
-    counter = 0
-    dict = File.new(File.expand_path "../naming/nouns.txt", __FILE__)
-    dict.each_line do |line|
-      if counter == a
-        return line.delete("\n")
-      end
-      counter+=1
-    end
+  def noun(first_letter)
+    list = dictionary('nouns')[first_letter]
+    list[rand(list.length - 1)]
   end
 
-  def get_adj_bound letter
-    higher = true
-    upper = 12586
-    lower = 0
-    a = 0
-    word = ""
-    done = false
-    until done
-      if higher
-        lower = a
-      else
-        upper = a
-      end
-      a = rand lower..upper
-      counter = 0
-      dict = File.new(File.expand_path "../naming/adj.txt", __FILE__)
-      dict.each_line do |line|
-        if counter == a
-          word = line.delete("\n")
-          break
-        end
-        counter+=1
-      end
-      if word[0] == letter
-        return word
-      elsif word[0] <= letter
-        higher = true
-      else
-        higher = false
-      end
-    end
-  end
-
-  def get_noun_bound letter
-    higher = true
-    upper = 20746
-    lower = 0
-    a = 0
-    word = ""
-    done = false
-    until done
-      if higher
-        lower = a
-      else
-        upper = a
-      end
-      a = rand lower..upper
-      counter = 0
-      dict = File.new(File.expand_path "../naming/nouns.txt", __FILE__)
-      dict.each_line do |line|
-        if counter == a
-          word = line.delete("\n")
-          break
-        end
-        counter+=1
-      end
-      if word[0] == letter
-        return word
-      elsif word[0] <= letter
-        higher = true
-      else
-        higher = false
-      end
-    end
+  def adjective(first_letter)
+    list = dictionary('adjectives')[first_letter]
+    list[rand(list.length - 1)]
   end
 end
