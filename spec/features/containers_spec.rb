8516b3d3550e1a80e45989aec8f076a030219040
Bastion Fennell <bastionfennell@thefrontside.net>
2013-08-01 11:24:46 -0500

    Added tests for containers

---
+++ spec/features/containers_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe "using the containers api" do
+  context "with credentials are present" do
+
+    include_context "netrc"
+
+    context "to create" do
+      When { VCR.use_cassette('containers/create') { run "rumm create container --name cruising-crouton" }}
+      Then { all_stdout =~ /created container/ }
+      And { last_exit_status.should eql 0 }
+    end
+
+    context "to show" do
+      When { VCR.use_cassette('containers/show') { run "rumm show container cruising-crouton" }}
+      Then { all_stdout =~ /cruising-crouton/ }
+      And { last_exit_status.should eql 0 }
+    end
+
+    context "to show all" do
+      When { VCR.use_cassette('containers/show-all') { run "rumm show containers" }}
+      Then { all_stdout =~ /Containers:/}
+      And { last_exit_status.should eql 0 }
+
+    context "to destroy" do
+      When { VCR.use_cassette('containers/destroy`') { run "rumm destroy container cruising-crouton" }}
+      Then { all_stdout =~ /Requested destruction/}
+      And { last_exit_status.should eql 0 }
+    end
+  end
+end
