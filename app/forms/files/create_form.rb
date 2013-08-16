eb380e1f91f09542fdb957a8fec3e988f2bf5238
Bastion Fennell <bastionfennell@thefrontside.net>
2013-08-16 15:26:15 -0500

    Kickin it old school with File.exists? to see if that works on Travis

--- app/forms/files/create_form.rb
+++ app/forms/files/create_form.rb
@@ -1,5 +1,5 @@
 class Files::CreateForm < MVCLI::Form
   input :file, Pathname, required: true, decode: ->(s) {Pathname(s)}
 
-  validates(:file, "File location must lead to a file") {|file| file.exist? }
+  validates(:file, "File location must lead to a file") {|file| File.exists? file }
 end
