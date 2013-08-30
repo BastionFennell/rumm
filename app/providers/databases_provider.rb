adeeabfd256844597a6ccae871a2811071927af7
Kyle Rames <kyle.rames@rackspace.com>
2013-08-30 14:43:13 -0500

    updating user agent string to include rumm version

--- app/providers/databases_provider.rb
+++ app/providers/databases_provider.rb
@@ -6,7 +6,8 @@ class DatabasesProvider
     options = {
       :rackspace_username => credentials.username,
       :rackspace_api_key  => credentials.api_key,
-      :rackspace_region   => credentials.rackspace_region
+      :rackspace_region   => credentials.rackspace_region,
+      :connection_options => {:headers => {"User-Agent" => "rumm/#{Rumm::VERSION} fog/#{Fog::VERSION}"}}
     }
     Fog::Rackspace::Databases.new(options).databases
   end
--- app/providers/volumes_provider.rb
+++ app/providers/volumes_provider.rb
@@ -8,6 +8,7 @@ class VolumesProvider
       :rackspace_username => credentials.username,
       :rackspace_api_key  => credentials.api_key,
       :rackspace_region   => credentials.rackspace_region
+      :connection_options => {:headers => {"User-Agent" => "rumm/#{Rumm::VERSION} fog/#{Fog::VERSION}"}}
     }
     Fog::Rackspace::BlockStorage.new(options).volumes
   end
