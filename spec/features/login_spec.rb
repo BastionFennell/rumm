144960d9e8ed7d26fa1e5703707e82229213f88c
Bastion Fennell <bastionfennell@thefrontside.net>
2013-09-18 14:09:01 -0500

    Set up login tests to work with new spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -15,22 +15,21 @@ describe "logging in" do
           username = $stdin.gets.chomp
           print "Password: "
           password = $stdin.noecho(&:gets).chomp
+          puts
+          print "Region: "
+          region = $stdin.gets.chomp
         end
         will_type username || "<username>"
         will_type password || "<password>"
+        will_type region   || "<region>"
         run_interactive "rumm login"
         stop_process @interactive
       end
     end
     Then {last_exit_status == 0}
 
-    context "places your login credentials in your .netrc" do
-      Given(:netrc) { Netrc.read["api.rackspace.com"] }
-      Given(:user) {netrc.first}
-      Given(:password) {netrc.last}
-
-      Then { user != nil }
-      And { password != nil }
+    context "creates .rummrc" do
+      Then { File.exists? "#{File.expand_path('~')}/.rummrc" }
     end
   end
 
@@ -38,24 +37,22 @@ describe "logging in" do
     When {run "rumm logout"}
     Then {last_exit_status == 0}
 
-    context "removes your login credentials from .netrc" do
-      Then do
-        n = Netrc.read
-        user, pass = n["api.rackspace.com"]
-        user == nil and pass == nil
+    context "deletes .rummrc" do
+      Then { !File.exists? "#{File.expand_path('~')}/.rummrc" }
+    end
+  end
+
+  context "interactively with invalid credentials" do
+    When do
+      VCR.use_cassette("authentication/unsuccessful-login") do
+        will_type "nil"
+        will_type "nil"
+        will_type "nil"
+        run_interactive "rum login"
+        stop_process @interactive
       end
     end
-    context "interactively with invalid credentials" do
-      When do
-        VCR.use_cassette("authentication/unsuccessful-login") do
-          will_type "nil"
-          will_type "nil"
-          run_interactive "rum login"
-          stop_process @interactive
-        end
-      end
-      Then {last_exit_status != 0}
-      And {all_stderr =~ /User could not be authenticated/}
-    end
+    Then {last_exit_status != 0}
+    And {all_stderr =~ /User could not be authenticated/}
   end
 end
