6093032cb0771f19caddd1bb097404d0d82d85e1
Kyle Rames <kyle.rames@rackspace.com>
2013-10-17 14:06:44 -0500

    adding HACK to display command help if invalid command is entered.

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require "mvcli/app"
 require "rumm/version"
+require "rumm/exceptions"
 
 module Rumm
   class App < MVCLI::App
     self.root = Pathname(__FILE__).dirname
+
+    def main(argv = ARGV.dup, input = $stdin, output = $stdout, log = $stderr, env = ENV.dup)
+      super.tap do |code|
+
+        # HACK: print out usage information if we can't the the command
+        if code == MVCLI::Middleware::ExitStatus::EX_USAGE
+          puts "\n"
+          super(%w[help commands], input, output, log, env) rescue code
+        end
+      end
+    end
+
   end
 end
